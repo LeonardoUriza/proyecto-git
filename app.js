7d864540dd540f408b94a53d3f0818041389c170
Leonardo Uriza <ur.leo16@gmail.com>
2023-11-03 20:26:49 -0500

    Login desarrollado: - Se creó el formulario HTML - Se hizo la lógica de autenticación ej JS

--- app.js
+++ app.js
@@ -1,6 +1,3 @@
-console.log("Inicio");
-console.log("Esta línea se cambió en local");
-console.log("Esta linea fue cambiada en remoto");
-console.log("Cambio 2");
-console.log("Cambio 3 en el remoto");
-console.log("Esta línea es nueva");
+function ingresar(){
+    console.log("Ingresando a la plataforma...");
+}
